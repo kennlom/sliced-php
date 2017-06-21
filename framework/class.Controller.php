2edb7c1000a0ed5ae383687e2a270019a48aba75
Kenn Lom Nguyen <kennlom@users.noreply.github.com>
2017-06-21 14:38:43 -0400

    Update class.Controller.php

--- framework/class.Controller.php
+++ framework/class.Controller.php
@@ -12,13 +12,13 @@
      * @license     http://www.updateflow.com/sliced-php/license
      * @see         http://www.updateflow.com/sliced-php
      */
-	class Controller
-	{
-		private $_layout	= 'default';
-		private $_template	= 'default';
-		private $_title		= '';
-		private $_arguments	= array();
-		private $_vars      = array();
+    class Controller
+    {
+        private $_layout	= 'default';
+        private $_template	= 'default';
+        private $_title		= '';
+        private $_arguments	= array();
+        private $_vars      = array();
 
 
 
@@ -27,22 +27,22 @@
          *
          * @return static
          */
-		public static function this() {
-			return new static;
-		}		
+        public static function this() {
+            return new static;
+        }		
 
-		public function setLayout($in=false) 		{ $this->_layout = $in; 	return $this; }		    # Set layout
-		public function setTemplate($in=false)		{ $this->_template = $in; 	return $this; }		    # Set template
-		public function setTitle($in='') 			{ $this->_title = $in; 		return $this; }		    # Set page title
-		public function setArguments($in=array()) 	{ $this->_arguments = $in; 	return $this; }		    # Set request arguments
-		public function getTitle() 					{ return $this->_title; } 						    # Get page title
-		public function getRequestParam($name) 		{ return @$_REQUEST[$name]; } 					    # Get request parameter
+        public function setLayout($in=false) 		{ $this->_layout = $in; 	return $this; }		    # Set layout
+        public function setTemplate($in=false)		{ $this->_template = $in; 	return $this; }		    # Set template
+        public function setTitle($in='') 			{ $this->_title = $in; 		return $this; }		    # Set page title
+        public function setArguments($in=array()) 	{ $this->_arguments = $in; 	return $this; }		    # Set request arguments
+        public function getTitle() 					{ return $this->_title; } 						    # Get page title
+        public function getRequestParam($name) 		{ return @$_REQUEST[$name]; } 					    # Get request parameter
         public function set($name, $value) 	        { $this->_vars[$name] = $value; return $this; }		# Set object variables
-		public function get($name) 					{ return @$this->_vars[$name]; } 					# Get object variables
+        public function get($name) 					{ return @$this->_vars[$name]; } 					# Get object variables
         public function getVariables()              { return $this->_vars; }
 
-		public function setSession($name, $value)	{ Engine::$SessionData[$name] = $value; }
-		public function removeSession($name)		{ unset($_SESSION[$name]); }
+        public function setSession($name, $value)	{ Engine::$SessionData[$name] = $value; }
+        public function removeSession($name)		{ unset($_SESSION[$name]); }
 
         /**
          * Get session value
@@ -62,9 +62,9 @@
          * @param int $i
          * @return mixed|null
          */
-		public function getArgument($i=0) {
-			return isset($this->_arguments[$i]) ? $this->_arguments[$i] : NULL;
-		}
+        public function getArgument($i=0) {
+            return isset($this->_arguments[$i]) ? $this->_arguments[$i] : NULL;
+        }
 	
         /**
          * Forward to another action to handle the request
@@ -72,51 +72,51 @@
          * @param       $action
          * @param array $args
          */
-		public function forward($action, $args=array())
+        public function forward($action, $args=array())
         {
-			$actionName = "execute{$action}";
+            $actionName = "execute{$action}";
 
-			if(method_exists($this, $actionName)) {
-				$this->$actionName($args);
-			}
-			else
-			{
-				Engine::class_exists('MainController', true);
+            if(method_exists($this, $actionName)) {
+                $this->$actionName($args);
+            }
+            else
+            {
+                Engine::class_exists('MainController', true);
 
-				// forward to 404 handler
-				(new MainController)->execute404();
-			}
-		}
+                // forward to 404 handler
+                (new MainController)->execute404();
+            }
+        }
 	
         /**
          * Redirect to another page
          *
          * @param $url
          */
-		public function redirect($url) {
-			header("Location: $url"); exit;
-		}
+        public function redirect($url) {
+            header("Location: $url"); exit;
+        }
 
         /**
-         * Render view
-         *
-         * @param string $view
-         * @param bool   $withLayout
-         */
-		public function render($view='', $withLayout=true) {
-			Engine::render($this, $withLayout ? $this->_layout : false, $view ? $view : $this->_template);
-		}
+        * Render view
+        *
+        * @param string $view
+        * @param bool   $withLayout
+        */
+        public function render($view='', $withLayout=true) {
+            Engine::render($this, $withLayout ? $this->_layout : false, $view ? $view : $this->_template);
+        }
 
         /**
-         * Render JSON
-         *
-         * @param $data
-         */
-		public function renderJSON($data) {
-			header('Content-Type: application/json');
-			echo json_encode($data);
-		}
+        * Render JSON
+        *
+        * @param $data
+        */
+        public function renderJSON($data) {
+            header('Content-Type: application/json');
+            echo json_encode($data);
+        }
 
-
-
-	}
+    
+    
+    }
