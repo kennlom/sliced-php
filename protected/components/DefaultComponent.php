af28c3718196273524840b2c8b9bc6b6d8291003
Kenn Lom Nguyen <kennlom@users.noreply.github.com>
2017-06-21 14:21:01 -0400

    Update DefaultComponent.php

--- protected/components/DefaultComponent.php
+++ protected/components/DefaultComponent.php
@@ -1,23 +1,23 @@
 <?php
 
-    namespace App;
+	namespace App;
 
 
-    /**
+    	/**
 	* Class DefaultComponent
-    *
-    * @package App
-    * @copyright   Copyright (c) 2017 Oanh, Inc. (http://www.updateflow.com)
-    * @license     http://www.updateflow.com/sliced-php/license
-    * @see         http://www.updateflow.com/sliced-php
-    */
+    	*
+    	* @package App
+    	* @copyright   Copyright (c) 2017 Oanh, Inc. (http://www.updateflow.com)
+    	* @license     http://www.updateflow.com/sliced-php/license
+    	* @see         http://www.updateflow.com/sliced-php
+    	*/
 	class DefaultComponent extends Controller
-    {
-        /**
-         * Execute component: /default/hello
-         */
+    	{
+        	/**
+         	* Execute component: /default/hello
+         	*/
 		public function executeHello()
-        {
+        	{
 			$this->render('components/index', false);
 		}
 	}
